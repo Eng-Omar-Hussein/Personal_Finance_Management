a508c4ff7fa894cca3d66f1f8c990c591d80df57
Ahmed Mohamed <108082265+3b3aty@users.noreply.github.com>
2023-04-26 02:19:59 +0200

    Update IncomeData.java

--- Personal_Finance_Management_System/src/main/java/incometracking/IncomeData.java
+++ Personal_Finance_Management_System/src/main/java/incometracking/IncomeData.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package incometracking;
 
 import User_Authentication.Cookies;
@@ -14,10 +10,6 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-/**
- *
- * @author Ahmed
- */
 public class IncomeData {
     private static final int ID = Cookies.getID();
     private String Category;
