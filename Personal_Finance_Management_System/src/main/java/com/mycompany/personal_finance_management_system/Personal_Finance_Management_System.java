d3eeec171106a46596451a32c4b15ad67fa0aa27
Eng\Mohamed Omar <119747408+M0hamed-0mar@users.noreply.github.com>
2023-05-10 19:07:18 +0300

    Update Personal_Finance_Management_System.java

--- Personal_Finance_Management_System/src/main/java/com/mycompany/personal_finance_management_system/Personal_Finance_Management_System.java
+++ Personal_Finance_Management_System/src/main/java/com/mycompany/personal_finance_management_system/Personal_Finance_Management_System.java
@@ -1,11 +1,13 @@
 package com.mycompany.personal_finance_management_system;
 
 import User_Authentication.Login_Page;
+import com.formdev.flatlaf.FlatLightLaf;
+
 
 public class Personal_Finance_Management_System {
 
     public static void main(String[] args) {
-
+        FlatLightLaf.setup();
         Login_Page obj = new Login_Page();
 
     }
