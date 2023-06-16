b481f39eab776715323cc8c009ed3e8fa8c88096
Anas Awaad <130825865+AnasAwaad@users.noreply.github.com>
2023-06-16 12:53:51 +0300

    Add files via upload

---
+++ Personal_Finance_Management_System/src/main/java/Budget_Planner_New/ConnectionDataBase.java
@@ -0,0 +1,30 @@
+
+package Budget_Planner_New;
+
+import Budget_planner.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class ConnectionDataBase {
+    public static Connection conn=null;
+    public static Connection connect() throws SQLException{
+        try {
+            Class.forName("org.sqlite.JDBC");
+            // Establish a connection to the database
+            
+            
+            
+            conn = DriverManager.getConnection("jdbc:sqlite:Budget.sqlite");
+            System.out.println("conected");
+        } catch (ClassNotFoundException ex) {
+            // Logger.getLogger(ConnectionDataBase.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        return conn;
+    }
+}
